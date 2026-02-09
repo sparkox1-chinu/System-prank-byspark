3398083d03bf1f0d5eff388d7c106483fe1a999b
sparkox1-chinu <sparkox19711@gmail.com>
2026-02-09 12:03:30 +0530

    Create Bg.js

---
+++ src/Bg.js
@@ -0,0 +1,96 @@
+var gradientColorsArray = new Array(
+  "#FFB6C1",
+  "#FFD700", 
+  "#FF7F50", 
+  "#87CEFA", 
+  "#DA70D6", 
+  "#98FB98", 
+  "#FFA07A", 
+  "#9370DB", 
+  "#00CED1", 
+  "#FF69B4", 
+  "#AFEEEE", 
+  "#40E0D0", 
+  "#E6E6FA", 
+  "#F08080", 
+  "#F0E68C", 
+  "#BA55D3", 
+  "#20B2AA", 
+  "#FFC0CB", 
+  "#7FFFD4", 
+  "#FFE4E1", 
+  "#B0E0E6", 
+  "#FA8072", 
+  "#E0FFFF", 
+  "#FFB347"  
+)
+
+
+var gradientTransitionStep = 0;
+
+
+var gradientColorIndices = [0, 1, 2, 3];
+
+
+var gradientTransitionSpeed = 0.004;
+
+function updateGradientBackground() {
+    if (typeof jQuery === 'undefined') return; 
+
+    
+    var currentLeftColorIndex = gradientColorIndices[0];
+    var nextLeftColorIndex = gradientColorIndices[1];
+    var currentRightColorIndex = gradientColorIndices[2];
+    var nextRightColorIndex = gradientColorIndices[3];
+
+    var currentLeftColorHex = gradientColorsArray[currentLeftColorIndex];
+    var nextLeftColorHex = gradientColorsArray[nextLeftColorIndex];
+    var currentRightColorHex = gradientColorsArray[currentRightColorIndex];
+    var nextRightColorHex = gradientColorsArray[nextRightColorIndex];
+
+    var inverseGradientStep = 1 - gradientTransitionStep;
+
+    function hexToRgb(hex) {
+        var r = parseInt(hex.slice(1, 3), 16);
+        var g = parseInt(hex.slice(3, 5), 16);
+        var b = parseInt(hex.slice(5, 7), 16);
+        return [r, g, b];
+    }
+
+    var [rLeftStart, gLeftStart, bLeftStart] = hexToRgb(currentLeftColorHex);
+    var [rLeftEnd, gLeftEnd, bLeftEnd] = hexToRgb(nextLeftColorHex);
+    var [rRightStart, gRightStart, bRightStart] = hexToRgb(currentRightColorHex);
+    var [rRightEnd, gRightEnd, bRightEnd] = hexToRgb(nextRightColorHex);
+
+    var redLeft = Math.round(inverseGradientStep * rLeftStart + gradientTransitionStep * rLeftEnd);
+    var greenLeft = Math.round(inverseGradientStep * gLeftStart + gradientTransitionStep * gLeftEnd);
+    var blueLeft = Math.round(inverseGradientStep * bLeftStart + gradientTransitionStep * bLeftEnd);
+    var leftGradientColor = "rgb(" + redLeft + "," + greenLeft + "," + blueLeft + ")";
+
+    var redRight = Math.round(inverseGradientStep * rRightStart + gradientTransitionStep * rRightEnd);
+    var greenRight = Math.round(inverseGradientStep * gRightStart + gradientTransitionStep * gRightEnd);
+    var blueRight = Math.round(inverseGradientStep * bRightStart + gradientTransitionStep * bRightEnd);
+    var rightGradientColor = "rgb(" + redRight + "," + greenRight + "," + blueRight + ")";
+
+    
+    $('#gradient').css({
+        background: "-webkit-gradient(linear, left top, right top, from(" + leftGradientColor + "), to(" + rightGradientColor + "))"
+    }).css({
+        background: "-moz-linear-gradient(left, " + leftGradientColor + " 0%, " + rightGradientColor + " 100%)"
+    });
+
+    gradientTransitionStep += gradientTransitionSpeed;
+    if (gradientTransitionStep >= 1) {
+        gradientTransitionStep %= 1;
+        gradientColorIndices[0] = gradientColorIndices[1];
+        gradientColorIndices[2] = gradientColorIndices[3];
+
+        gradientColorIndices[1] = (gradientColorIndices[1] + Math.floor(1 + Math.random() * (gradientColorsArray.length - 1))) % gradientColorsArray.length;
+        gradientColorIndices[3] = (gradientColorIndices[3] + Math.floor(1 + Math.random() * (gradientColorsArray.length - 1))) % gradientColorsArray.length;
+    }
+}
+
+
+setInterval(updateGradientBackground, 50); // It Means 10 milliseconds 
+
+
